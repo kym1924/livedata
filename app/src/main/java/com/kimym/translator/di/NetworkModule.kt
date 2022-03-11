a55f6c6d52e88a079ae4ff2a5700be929f09011f
kym1924 <kym1924@naver.com>
2022-03-11 13:33:38 +0900

    Rename RetrofitModule to NetworkModule

--- app/src/main/java/com/kimym/translator/di/NetworkModule.kt
+++ app/src/main/java/com/kimym/translator/di/NetworkModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitModule {
+object NetworkModule {
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit =
